5faecb4afd9c9eb970b3a3e588fefc45e55cc4e0
MarechalBanane <faure_g@hotmail.com>
2014-12-06 18:09:23 +0100

    persos bougent et sautent !

--- Boucucle/Assets/Scripts/LightGuy.cs
+++ Boucucle/Assets/Scripts/LightGuy.cs
@@ -1,24 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LightGuy : MonoBehaviour {
 
     private Rigidbody2D m_RigidBody;
+    private bool        m_CanJump = false;
+    private List<GameObject> m_CollidingStuff;
+
+    public float m_JumpImpulse;
 
 	// Use this for initialization
 	void Start () {
         m_RigidBody = this.rigidbody2D;
+        m_CanJump = false;
+        m_CollidingStuff = new List<GameObject>();
         if (m_RigidBody == null)
         {
             Debug.LogError("No rigidbody 2D attached to lightguy !");
         }
 	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.bounds.max.y < this.collider2D.bounds.min.y)
+        {
+            m_CollidingStuff.Add(coll.gameObject);
+            m_CanJump = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        m_CollidingStuff.Remove(coll.gameObject);
+        m_CanJump = m_CollidingStuff.Count > 0;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        float axisValue = Input.GetAxis("HorizontalP1");
+        float axisValue = Input.GetAxis("HorizontalP1Joy");
         m_RigidBody.AddForce(new Vector2(axisValue, 0), ForceMode2D.Impulse);
+        if ( Input.GetKeyDown ( KeyCode.Joystick1Button0 ) && m_CanJump)
+        {
+            m_CanJump = false;
+            m_RigidBody.AddForce(new Vector2(0, m_JumpImpulse), ForceMode2D.Impulse);
+        }
 
 	}
 }
--- Boucucle/Assets/Scripts/Nemesis.cs
+++ Boucucle/Assets/Scripts/Nemesis.cs
@@ -1,24 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Nemesis : MonoBehaviour {
 
     private Rigidbody2D m_RigidBody;
+    private bool m_CanJump = false;
+    private List<GameObject> m_CollidingStuff;
+
+    public float m_JumpImpulse;
 
 	// Use this for initialization
 	void Start () {
         m_RigidBody = this.rigidbody2D;
+        m_CanJump = false;
+        m_CollidingStuff = new List<GameObject>();
         if (m_RigidBody == null)
         {
             Debug.LogError("No rigidbody 2D attached to nemesis !");
         }
-	}
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.bounds.max.y < this.collider2D.bounds.min.y)
+        {
+            m_CollidingStuff.Add(coll.gameObject);
+            m_CanJump = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        m_CollidingStuff.Remove(coll.gameObject);
+        m_CanJump = m_CollidingStuff.Count > 0;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        float axisValue = Input.GetAxis("HorizontalP2");
+        float axisValue = Input.GetAxis("HorizontalP2Joy");
         m_RigidBody.AddForce(new Vector2(axisValue, 0), ForceMode2D.Impulse);
 
+        if (Input.GetKeyDown(KeyCode.Joystick2Button0) && m_CanJump)
+        {
+            m_CanJump = false;
+            m_RigidBody.AddForce(new Vector2(0, m_JumpImpulse), ForceMode2D.Impulse);
+        }
 	}
 }
