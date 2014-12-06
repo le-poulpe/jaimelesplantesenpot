47e211ba863396e3cae0cc96351b54ca22b72290
MarechalBanane <faure_g@hotmail.com>
2014-12-06 20:03:10 +0100

    player can blast

--- Boucucle/Assets/Scripts/LightGuy.cs
+++ Boucucle/Assets/Scripts/LightGuy.cs
@@ -14,10 +14,12 @@ public class LightGuy : MonoBehaviour {
     public float m_MoveSpeed = 1;
     public float m_MaxEnergy = 100;
     public float m_EnergyLossPerSecond = 0.1f;
+    public float m_BlastSuckPerSecond = 15;
 
-    public Light m_Light = null;
-    public float m_MinAuraRange = 1;
-    public float m_MaxAuraRange = 2;
+    public Light m_AuraLight = null;
+    public Light m_BlastLight = null;
+    public float m_MinAuraIntensity = 0;
+    public float m_MaxAuraIntensity = 2;
 
 	// Use this for initialization
 	void Start () {
@@ -25,10 +27,21 @@ public class LightGuy : MonoBehaviour {
         m_CanJump = false;
         m_CollidingStuff = new List<GameObject>();
         m_Energy = m_MaxEnergy;
+
         if (m_RigidBody == null)
         {
             Debug.LogError("No rigidbody 2D attached to lightguy !");
         }
+        if (m_AuraLight == null)
+        {
+            Debug.LogError("No aura light attached to lightguy !");
+        }
+        if (m_BlastLight == null)
+        {
+            Debug.LogError("No blast light 2D attached to lightguy !");
+        }
+        else
+            m_BlastLight.enabled = false;
 	}
 
     void OnCollisionEnter2D(Collision2D coll)
@@ -63,15 +76,33 @@ public class LightGuy : MonoBehaviour {
             float axisValue = Input.GetAxis("HorizontalP1Joy");
             m_RigidBody.AddForce(new Vector2(axisValue * m_MoveSpeed, 0), ForceMode2D.Impulse);
 
+            // Jump
             if (Input.GetKeyDown(KeyCode.Joystick1Button0) && m_CanJump)
             {
                 m_CanJump = false;
                 m_RigidBody.AddForce(new Vector2(0, m_JumpImpulse), ForceMode2D.Impulse);
             }
 
+            //Shoot
+            if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+            {
+                // TODO
+            }
+
+            //Blast
+            if (Input.GetAxis("BlastP1Joy") < 0) // xbox left trigger
+            {
+                m_BlastLight.enabled = true;
+            }
+            else
+            {
+                m_BlastLight.enabled = false;
+            }
+
+
             // update light
-            if (m_Light != null)
-                m_Light.intensity = Mathf.Lerp(m_MinAuraRange, m_MaxAuraRange, m_Energy / m_MaxEnergy);
+            if (m_AuraLight != null)
+                m_AuraLight.intensity = Mathf.Lerp(m_MinAuraIntensity, m_MaxAuraIntensity, m_Energy / m_MaxEnergy);
 
             // die slowly
             m_Energy -= m_EnergyLossPerSecond * Time.deltaTime;
@@ -79,6 +110,12 @@ public class LightGuy : MonoBehaviour {
             // die less slowly if in contact with nemesis
             if (m_AttackingNemesis != null)
                 m_Energy -= m_AttackingNemesis.m_EnergySuckPerSecond * Time.deltaTime;
+
+            // die less slowly if blasting
+            if (m_BlastLight.enabled)
+                m_Energy -= m_BlastSuckPerSecond * Time.deltaTime;
         }
+        else if (m_BlastLight.enabled)
+            m_BlastLight.enabled = false;
 	}
 }
