c4d08eaf4cb304658d5a0da49e0cb14471603688
MarechalBanane <faure_g@hotmail.com>
2014-12-06 16:32:56 +0100

    first lightguy controller

---
+++ Boucucle/Assets/Scripts/LightGuy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightGuy : MonoBehaviour {
+
+    private Rigidbody2D m_RigidBody;
+
+	// Use this for initialization
+	void Start () {
+        m_RigidBody = this.rigidbody2D;
+        if (m_RigidBody == null)
+        {
+            Debug.LogError("No rigidbody 2D attached to lightguy !");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float axisValue = Input.GetAxis("Horizontal");
+        m_RigidBody.AddForce(new Vector2(axisValue, 0), ForceMode2D.Impulse);
+
+	}
+}
