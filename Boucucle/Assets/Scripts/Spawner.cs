32946c7905778906752348863b526db8ec973a70
MarechalBanane <faure_g@hotmail.com>
2014-12-07 14:36:28 +0100

    spawner script

---
+++ Boucucle/Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public Vector2[] m_SpawnPoints;
+	public GameObject m_LightGuyPrefab;
+	public GameObject m_NemesisPrefab;
+	public GameObject m_PotDeFleurPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        if (m_SpawnPoints.Length < 3)
+        {
+            Debug.LogError("Need at least 3 spawn points !");
+        }
+
+        // Fisher Yates
+		for (int i = 0; i < m_SpawnPoints.Length; ++i)
+        {
+            int rand = Random.Range(i, m_SpawnPoints.Length);
+            Vector2 temp = m_SpawnPoints[i];
+            m_SpawnPoints[i] = m_SpawnPoints[rand];
+            m_SpawnPoints[rand] = temp;
+        }
+
+        GameObject.Instantiate(m_LightGuyPrefab, m_SpawnPoints[0], Quaternion.identity);
+        GameObject.Instantiate(m_NemesisPrefab, m_SpawnPoints[1], Quaternion.identity);
+        GameObject.Instantiate(m_PotDeFleurPrefab, m_SpawnPoints[2], Quaternion.identity);
+	}
+}
